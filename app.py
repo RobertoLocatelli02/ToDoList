7b639f59b99c2e1f2a302fa10f462cc6cbec91b1
Roberto Locatelli <80109160+RobertoLocatelli02@users.noreply.github.com>
2024-08-26 22:38:40 -0300

    WIP 20240826

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'roberto.locatelli.testes@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Locatelli123'
+app.config['MAIL_PASSWORD'] = 'teste'
 
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 
